ae2efcd152199b261414f0ba62c34f590888f55a
sakshigrover-1998 <sakshi.codeq@gmail.com>
2021-04-26 12:53:33 +0530

    code added

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+const gulp = require("gulp");
+const gap = require("gulp-append-prepend");
+
+gulp.task("licenses", async function () {
+  // this is to add Creative Tim licenses in the production mode for the minified js
+  gulp
+    .src("build/static/js/*chunk.js", { base: "./" })
+    .pipe(
+      gap.prependText(`/*!
+
+=========================================================
+* BLK Design System React - v1.2.0
+=========================================================
+
+
+
+=========================================================
+
+
+
+*/`)
+    )
+    .pipe(gulp.dest("./", { overwrite: true }));
+
+  // this is to add Creative Tim licenses in the production mode for the minified html
+  gulp
+    .src("build/index.html", { base: "./" })
+    .pipe(
+      gap.prependText(`<!--
+
+=========================================================
+* BLK Design System React - v1.2.0
+=========================================================
+
+
+
+=========================================================
+
+
+
+-->`)
+    )
+    .pipe(gulp.dest("./", { overwrite: true }));
+
+  // this is to add Creative Tim licenses in the production mode for the minified css
+  gulp
+    .src("build/static/css/*chunk.css", { base: "./" })
+    .pipe(
+      gap.prependText(`/*!
+
+=========================================================
+* BLK Design System React - v1.2.0
+=========================================================
+
+
+
+=========================================================
+
+
+
+*/`)
+    )
+    .pipe(gulp.dest("./", { overwrite: true }));
+  return;
+});
---
+++ src/views/examples/LandingPage.js
@@ -0,0 +1,338 @@
+/*!
+
+=========================================================
+* BLK Design System React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/blk-design-system-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/blk-design-system-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// react plugin used to create charts
+import { Line } from "react-chartjs-2";
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  ListGroupItem,
+  ListGroup,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+
+// core components
+import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
+import Footer from "components/Footer/Footer.js";
+import girl from "./girl.png";
+import bigChartData from "variables/charts.js";
+
+export default function LandingPage() {
+  React.useEffect(() => {
+    document.body.classList.toggle("landing-page");
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      document.body.classList.toggle("landing-page");
+    };
+  },[]);
+  return (
+    <>
+      <ExamplesNavbar />
+      <div className="wrapper">
+        <div className="page-header">
+          <img
+            alt="..."
+            className="path"
+            src={require("assets/img/blob.png").default}
+          />
+          <img
+            alt="..."
+            className="path2"
+            src={require("assets/img/path2.png").default}
+          />
+          <img
+            alt="..."
+            className="shapes triangle"
+            src={require("assets/img/triunghiuri.png").default}
+          />
+          <img
+            alt="..."
+            className="shapes wave"
+            src={require("assets/img/waves.png").default}
+          />
+          <img
+            alt="..."
+            className="shapes squares"
+            src={require("assets/img/patrat.png").default}
+          />
+          <img
+            alt="..."
+            className="shapes circle"
+            src={require("assets/img/cercuri.png").default}
+          />
+          <div className="content-center">
+            <Row className="row-grid justify-content-between align-items-center text-left">
+              <Col lg="6" md="6">
+                <h1 className="text-white">
+                  We make understanding technology <br />
+                  <span className="text-white">easy</span>
+                </h1>
+                <p className="text-white mb-3">
+                 InfoBytes is here to make your life easy with simplified technology news and interesting facts to keep you updated and relevant in this ever-changing world
+                </p>
+              
+                <div className="btn-wrapper">
+                  <div className="button-container">
+                    <Button
+                      className="btn-icon btn-simple btn-round btn-neutral"
+                      color="default"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-twitter" />
+                    </Button>
+                    <Button
+                      className="btn-icon btn-simple btn-round btn-neutral"
+                      color="default"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-dribbble" />
+                    </Button>
+                    <Button
+                      className="btn-icon btn-simple btn-round btn-neutral"
+                      color="default"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-facebook" />
+                    </Button>
+                  </div>
+                </div>
+              </Col>
+              <Col lg="4" md="5">
+                <img
+                  alt="..."
+                  className="img-fluid"
+                  src={require("assets/img/front.png").default}
+                />
+              </Col>
+            </Row>
+          </div>
+        </div>
+        <section className="section section-lg">
+          <section className="section">
+            <img
+              alt="..."
+              className="path"
+              src={require("assets/img/path4.png").default}
+            />
+            <Container>
+              <Row className="row-grid justify-content-between">
+                <Col className="mt-lg-5" md="5">
+                  <img src={girl} />
+                </Col>
+                <Col md="6">
+                  <div className="pl-md-5">
+                    <h1>
+                      What we have in <br />
+                      Store
+                    </h1>
+                    <p>
+                      Technology is indispensable and it dominates all aspects of our lives.  
+                      It has created a revolution in how we work, play, and live our lives, 
+                      and it will continue to grow as long as humans continue to advance in their capabilities.
+                       Technology improves day by day , what was the latest and greatest yesterday is now outdated.
+                    </p>
+                    <br />
+                    <p>
+                      With daily "Byte" sized information about upcoming technologies, 
+                      their basic working, their influence on your life and how it's changing the world,
+                       keep yourself one step ahead of the crowd and grab the opportunities that are waiting for you!
+                    </p>
+                    <br />
+                    
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </section>
+        </section>
+        <section className="section section-lg">
+          <img
+            alt="..."
+            className="path"
+            src={require("assets/img/path4.png").default}
+          />
+          <img
+            alt="..."
+            className="path2"
+            src={require("assets/img/path5.png").default}
+          />
+          <img
+            alt="..."
+            className="path3"
+            src={require("assets/img/path2.png").default}
+          />
+          <Container>
+            <Row className="justify-content-center">
+              <Col lg="12">
+                <h1 className="text-center">Daily Dose of InfoBytes</h1>
+                <Row className="row-grid justify-content-center">
+                  <Col lg="3">
+                    <div className="info">
+                     
+                     
+                    </div>
+                  </Col>
+                  <Col lg="3">
+                    <div className="info">
+                   
+               
+                    </div>
+                  </Col>
+                  <Col lg="3">
+                    <div className="info">
+                      
+                      
+                    </div>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+        <section className="section section-lg section-safe">
+          <img
+            alt="..."
+            className="path"
+            src={require("assets/img/path5.png").default}
+          />
+          <Container>
+            <Row className="row-grid justify-content-between">
+              <Col md="5">
+                <img
+                  alt="..."
+                  className="img-fluid floating"
+                  src={require("assets/img/tech.png").default}
+                />
+                <Card className="card-stats bg-danger">
+                  <CardBody>
+                    <div className="justify-content-center">
+                      <div className="numbers">
+                        <CardTitle tag="p">100%</CardTitle>
+                        <p className="card-category text-white">Authentic</p>
+                      </div>
+                    </div>
+                  </CardBody>
+                </Card>
+                <Card className="card-stats bg-info">
+                  <CardBody>
+                    <div className="justify-content-center">
+                      <div className="numbers">
+                        <CardTitle tag="p"></CardTitle>
+                        <p className="card-category text-white">
+                          Designed from Scratch
+                        </p>
+                      </div>
+                    </div>
+                  </CardBody>
+                </Card>
+                <Card className="card-stats bg-default">
+                  <CardBody>
+                    <div className="justify-content-center">
+                      <div className="numbers">
+                        <CardTitle tag="p"></CardTitle>
+                        <p className="card-category text-white">Short and Crisp</p>
+                      </div>
+                    </div>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col md="6">
+                <div className="px-md-5">
+                  <hr className="line-success" />
+                  <h3>Awesome features</h3>
+                  <p>
+                    some text
+                    
+                  </p>
+                  
+                  <ul className="list-unstyled mt-5">
+                    <li className="py-2">
+                      <div className="d-flex align-items-center">
+                        <div className="icon icon-success mb-2">
+                          <i className="tim-icons icon-vector" />
+                        </div>
+                        <div className="ml-3">
+                          <h6>Carefully crafted content</h6>
+                        </div>
+                      </div>
+                    </li>
+                    <li className="py-2">
+                      <div className="d-flex align-items-center">
+                        <div className="icon icon-success mb-2">
+                          <i className="tim-icons icon-tap-02" />
+                        </div>
+                        <div className="ml-3">
+                          <h6>Amazing Illustrations</h6>
+                        </div>
+                      </div>
+                    </li>
+                    <li className="py-2">
+                      <div className="d-flex align-items-center">
+                        <div className="icon icon-success mb-2">
+                          <i className="tim-icons icon-single-02" />
+                        </div>
+                        <div className="ml-3">
+                          <h6>Covering various domains</h6>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+        
+        <section className="section section-lg section-coins">
+          <img
+            alt="..."
+            className="path"
+            src={require("assets/img/path3.png").default}
+          />
+          <Container>
+            <Row>
+              <Col md="4">
+                <hr className="line-info" />
+                <h1>
+                  Subscribe to our newsletter!
+                </h1>
+              </Col>
+            </Row>
+          
+            <Button color="primary" size="lg" href="https://infobytes.substack.com">
+              Subscribe
+            </Button>
+           
+              
+          </Container>
+        </section>
+        <Footer />
+      </div>
+    </>
+  );
+}
